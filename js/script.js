7c159752cd68c7b0ca4cccfcd5275f518624464a
Ivan <matteraivam@gmail.com>
2020-07-16 16:01:56 +0200

    fatto quasi tutto, non mi funge il classname ma ora lo fixo

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+ document.getElementById("genera").addEventListener("click" , function() {
+      var x = document.getElementById("nome").value;
+      document.getElementById("nome-biglietto").innerHTML = x;
+      var km = document.getElementById("km").value;
+      var prezzo = km * 0.21;
+      var prezzoFinale = prezzo;
+      var eta = document.getElementById("eta").value;
+      if (eta == "minorenne") {
+        prezzoFinale = prezzo * 0.8;
+      } else if (eta == "over") {
+        prezzoFinale = prezzo * 0.6;
+      }
+      document.getElementById("sconto").innerHTML = "sconto" + "<br>" + eta;
+      var finale = prezzoFinale.toFixed(2);
+      document.getElementById("price").innerHTML = finale;
+      document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * (9 - 1) + 1);
+      document.getElementById("vagone").innerHTML = Math.floor(Math.random() * (99999 - 90000) + 90000);
+});
